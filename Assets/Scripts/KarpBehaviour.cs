fbca11f56ce687c868c0f299f46d8440a8ca9769
Bartek <bartlomiejgrzegorek@gmail.com>
2016-12-21 23:19:56 +0100

    jeszcze mniejsza scena; wiecej ryb (klony); drugi wodorost i textura

---
+++ Assets/Scripts/KarpBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KarpBehaviour : MonoBehaviour {
+	private bool gravityReversed = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gravityReversed)
+        {
+            //GetComponent<Rigidbody>().AddForce(Vector3.up, Physics.gravity.magnitude);
+        }
+	}
+}
