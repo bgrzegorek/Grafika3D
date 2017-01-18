ee4602763cf843e1cf9375de1ceee7aab00bcb39
Marcin Mantke <mentosso@gmail.com>
2017-01-18 17:39:41 +0100

    dodatkowa kamera

---
+++ Assets/ChangeCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class ChangeCamera : MonoBehaviour {
+	// Define Cams
+	public Camera camera1;
+	public Camera camera2;
+
+    void Start() {
+        camera1.enabled = true;
+        camera2.enabled = false;
+    }
+
+    void Update() {
+        // Check for input and swap accordingly
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            camera1.enabled = !camera1.enabled;
+            camera2.enabled = !camera2.enabled;
+        }
+    }
+}
